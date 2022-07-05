ff5adea64ec46e6f3732dd70ea4f3239d32cd0df
Hyun Sung Cho <hcho774@gmail.com>
2022-07-05 13:39:51 -0400

    login & signup components and page implemented in frontend

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div>
+      <div>
+        <form action="">
+          <p>Username</p>
+          <input
+            type="username"
+            name="username"
+            placeholder="Enter your username"
+          />
+          <input
+            type="password"
+            name="passwored"
+            placeholder="Enter your password"
+          />
+          <button>submit</button>
+          <p>
+            Don't have an account? &nbsp;
+            <button>Sign Up</button>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
