7bd3f426481c5c98f79b83fb4a4f776485040975
Hyun Sung Cho <hcho774@gmail.com>
2022-07-05 13:24:56 -0400

    front end client pages,components updated

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Question from "../../components/Question";
+const Home = () => {
+  return (
+    <div>
+      <div className="homecontainer">
+        <Question />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
