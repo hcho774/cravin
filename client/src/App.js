f17a121deb1632605888736da5a7ed5923ca94b2
Hyun Sung Cho <hcho774@gmail.com>
2022-07-14 15:52:04 -0400

    presentaion

--- client/src/App.js
+++ client/src/App.js
@@ -39,44 +39,44 @@ function App() {
       }
     });
 
-  const deleterooms = () => {
-    fetch(`/deleterooms/`, { method: "DELETE" }).then((r) => {
-      if (r.ok) {
-        console.log("all rooms and messages have been deleted");
-      }
-    });
-  };
+  // const deleterooms = () => {
+  //   fetch(`/deleterooms/`, { method: "DELETE" }).then((r) => {
+  //     if (r.ok) {
+  //       console.log("all rooms and messages have been deleted");
+  //     }
+  //   });
+  // };
 
-  const resetmatches = () => {
-    fetch("/resetmatches/", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ matches: 0 }),
-    }).then((r) => {
-      if (r.ok) {
-        r.json().then((user) => {
-          getUser();
-          console.log("matches has been resetted");
-        });
-      } else {
-        r.json().then((err) => {
-          // setErrors(err.errors);
-        });
-      }
-    });
-  };
+  // const resetmatches = () => {
+  //   fetch("/resetmatches/", {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ matches: 0 }),
+  //   }).then((r) => {
+  //     if (r.ok) {
+  //       r.json().then((user) => {
+  //         getUser();
+  //         console.log("matches has been resetted");
+  //       });
+  //     } else {
+  //       r.json().then((err) => {
+  //         // setErrors(err.errors);
+  //       });
+  //     }
+  //   });
+  // };
 
   useEffect(() => {
     getUser();
 
-    const interval = setInterval(() => {
-      resetmatches();
-      deleterooms();
-    }, 100000);
+    // const interval = setInterval(() => {
+    //   resetmatches();
+    //   deleterooms();
+    // }, 100000);
 
-    return () => clearInterval(interval);
+    // return () => clearInterval(interval);
   }, []);
 
   if (!user) return <Home user={user} setUser={setUser} navigate={navigate} />;
